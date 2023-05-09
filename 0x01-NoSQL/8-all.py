c3b1fc1a995b62099741d886feced365d88cba06
mdjibo2 <justpourmestest00@gmail.com>
2023-05-09 16:50:32 -0700

    updates

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all documents in a collection
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    list all collections
+    """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
